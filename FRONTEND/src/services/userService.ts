997c87d0cf27f261db949e85f99e11fd45cebc5d
Daniel Rogowski <daniel.rogowski@onet.pl>
2025-03-19 16:23:29 +0100

    PTR-12: Add Github Actions workflow for automated testing

--- FRONTEND/src/services/userService.ts
+++ FRONTEND/src/services/userService.ts
@@ -2,15 +2,20 @@ import { User } from "../models/User";
 
 // Login exist user
 export const loginUser = async (): Promise<User> => {
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = "localhost:5000"; // dummy, to be changed later
   const response = await fetch(`${API_URL}/login`);
   return response.json();
 };
 
 // Register new user
 export const registerUser = async (): Promise<User> => {
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = "localhost:5000"; // dummy, to be changed later
   const response = await fetch(`${API_URL}/register`);
   return response.json();
 }
 
+export const fetchUsers = async (): Promise<User[]> => {
+  const API_URL = "localhost:5000"; // dummy, to be changed later
+  const response = await fetch(`${API_URL}/users`);
+  return response.json();
+}
--- FRONTEND/src/views/HomeView.tsx
+++ FRONTEND/src/views/HomeView.tsx
@@ -1,5 +1,7 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+// caused import error when calling "npm run build", to be fixed later
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomeView = () => {
 return (
