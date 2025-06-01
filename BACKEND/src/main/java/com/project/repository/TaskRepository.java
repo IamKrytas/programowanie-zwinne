ea4cf6c8adaedcfec5cb0dab3e04cbab114d7012
Daniel Rogowski <daniel.rogowski@onet.pl>
2025-06-01 23:48:04 +0200

    PTR-80: Setup skeleton directory for integration testing

--- BACKEND/src/main/java/com/project/repository/TaskRepository.java
+++ BACKEND/src/main/java/com/project/repository/TaskRepository.java
@@ -1,7 +1,14 @@
 package com.project.repository;
 
 import com.project.model.Task;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends MongoRepository<Task, String> {
+    List<Task> findByAssignedStudentId(String studentId, Pageable pageable);
+    List<Task> findByTeacherId(String userId, Pageable pageable);
+    List<Task> findByTeacherId(String userId);
 }
