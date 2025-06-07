3fa7e67504bf278c4b9d9732ec5cddfa9e46a4d0
IamKrytas <kedzierski.aleksander@gmail.com>
2025-06-07 18:21:32 +0200

    Add routes for task and project management

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -6,6 +6,8 @@ import HomePageView from './views/HomePageView';
 import Navbar from './components/Navbar';
 import AdminPanelView from './views/AdminPanelView';
 import ChatView from "./views/ChatView.tsx";
+import ProjectView from './views/ProjectView.tsx';
+import TaskView from './views/TaskView.tsx';
 // import Footer from './components/Footer';
 
 
@@ -29,6 +31,8 @@ function App() {
                 ) : (
                     <>
                         <Route path="/home" element={<HomePageView />} />
+                        <Route path="/projects" element={<ProjectView />} />
+                        <Route path="/tasks" element={<TaskView />} />
                         <Route path="/chat" element={<ChatView />} />
                         <Route path="*" element={<Navigate to="/home" />} />
                         {isAdmin && (
