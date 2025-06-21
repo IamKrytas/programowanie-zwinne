56ca81c8304ba558babb0121809b3b63de3d5a78
IamKrytas <kedzierski.aleksander@gmail.com>
2025-06-21 22:14:46 +0200

    Add all admin student services

---
+++ FRONTEND/src/services/adminStudentService.ts
@@ -0,0 +1,89 @@
+import { Student } from "../models/Student";
+
+export const getAllStudentsService = async (): Promise<Student[]> => {
+    const token = sessionStorage.getItem("accessToken");
+    const API_URL = "http://localhost:8080";
+    const response = await fetch(`${API_URL}/api/v1/admin/student?offset=0&limit=10`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+    });
+    if (!response.ok) {
+        throw new Error(`Błąd: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+export const getStudentByIdService = async (id: string): Promise<Student> => {
+    const token = sessionStorage.getItem("accessToken");
+    const API_URL = "http://localhost:8080";
+    const response = await fetch(`${API_URL}/api/v1/admin/student/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+    });
+    if (!response.ok) {
+        throw new Error(`Błąd: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+export const createStudentService = async (student: Student): Promise<Student> => {
+    const token = sessionStorage.getItem("accessToken");
+    const API_URL = "http://localhost:8080";
+    const response = await fetch(`${API_URL}/api/v1/admin/student`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(student),
+    });
+    if (!response.ok) {
+        throw new Error(`Błąd: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+export const modifyStudentByIdService = async (student: Student): Promise<Student> => {
+    const token = sessionStorage.getItem("accessToken");
+    const API_URL = "http://localhost:8080";
+    const response = await fetch(`${API_URL}/api/v1/admin/student/${student.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(student),
+    });
+    if (!response.ok) {
+        throw new Error(`Błąd: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+export const deleteStudentByIdService = async (id: string): Promise<void> => {
+    const token = sessionStorage.getItem("accessToken");
+    const API_URL = "http://localhost:8080";
+    const response = await fetch(`${API_URL}/api/v1/admin/student/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+    });
+    if (!response.ok) {
+        throw new Error(`Błąd: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+};
